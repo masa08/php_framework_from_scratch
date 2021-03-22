c58ba953c7a723e3cf381823afc9228cb0eb19fc
masa08 <ushimasarug08@gmail.com>
2021-03-22 22:17:40 +0900

    :sparkles: Add Router class

---
+++ application/core/Router.php
@@ -0,0 +1,49 @@
+<?php
+
+class Router
+{
+  protected $routes;
+
+  public function __construct($definitions)
+  {
+    $this->routes = $this->compileRoutes($definitions);
+  }
+
+  public function compileRoutes($definitions)
+  {
+    $routes = array();
+
+    foreach ($definitions as $url => $params) {
+      $token = explode('/', ltrim($url, '/'));
+      foreach ($token as $i => $token) {
+        if (0 === strpos($token, ':')) {
+          $name = substr($token, 1);
+          $token = '(?P<' . $name . '>[^/]+)';
+        }
+        $token[$i] = $token;
+      };
+
+      $pattern = '/' . implode('/', $token);
+      $routes[$pattern] = $params;
+    }
+
+    return $routes;
+  }
+
+  public function resolve($path_info)
+  {
+    if ('/' !== substr($path_info, 0, 1)) {
+      $path_info = '/' . $path_info;
+    }
+
+    foreach ($this->routes as $pattern => $params) {
+      if (preg_match('#^' . $pattern . '$#', $path_info, $matches)) {
+        $params = array_merge($params, $matches);
+
+        return $params;
+      }
+    }
+
+    return false;
+  }
+}
