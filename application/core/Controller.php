68293d4a30f15b0195c79be86d9d6ba3cde8470a
masa08 <ushimasarug08@gmail.com>
2021-03-24 08:13:09 +0900

    :sparkles: Implement basic framework

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -31,6 +31,10 @@ abstract class Controller
       $this->forward404();
     }
 
+    if ($this->needsAuthentication($action) && !$this->session->isAuthenticated()) {
+      throw new UnauthorizedActionException();
+    }
+
     $content = $this->$action_method($params);
 
     return $content;
@@ -73,5 +77,48 @@ abstract class Controller
 
     $this->response->setStatusCode(302, 'Found');
     $this->response->setHttpHeader('Location', $url);
+  }
+
+  protected function generateCsrfToken($form_name)
+  {
+    $key = 'csrf_tokens/' . $form_name;
+    $tokens = $this->session->get($key, array());
+    if (count($tokens) >= 10) {
+      array_shift($tokens);
+    }
+
+    $token = sha1($form_name . session_id() . microtime());
+    $tokens[] = $token;
+
+    $this->session->set($key, $tokens);
+
+    return $token;
+  }
+
+  protected function checkCsrfToken($form_name, $token)
+  {
+    $key = 'csrf_tokens/' . $form_name;
+    $tokens = $this->session->get($key, array());
+
+    if (false !== ($pos = array_search($token, $tokens, true))) {
+      unset($tokens[$pos]);
+      $this->session->set($key, $tokens);
+
+      return true;
+    }
+
+    return false;
+  }
+
+  protected function needsAuthentication($action)
+  {
+    if (
+      $this->auth_actions === true
+      || (is_array($this->auth_actions) && in_array($action, $this->auth_actions))
+    ) {
+      return true;
+    }
+
+    return false;
   }
 }
